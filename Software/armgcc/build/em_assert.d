57c2044b9f6ad68f1c7eee7b24df7d5afad449eb
Peter Loes <54844299+peterloes@users.noreply.github.com>
2019-10-08 13:59:53 +0200

    Add files via upload

---
+++ Software/armgcc/build/em_assert.d
@@ -0,0 +1,3 @@
+build/em_assert.o: ../emlib/src/em_assert.c ../emlib/inc/em_assert.h
+
+../emlib/inc/em_assert.h:
---
+++ Software/armgcc/build/em_gpio.d
@@ -0,0 +1,112 @@
+build/em_gpio.o: ../emlib/src/em_gpio.c ../emlib/inc/em_gpio.h \
+ ../Device/EnergyMicro/EFM32G/Include/em_device.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g230f128.h \
+ ../CMSIS/Include/core_cm3.h ../CMSIS/Include/core_cmInstr.h \
+ ../CMSIS/Include/core_cmFunc.h \
+ ../Device/EnergyMicro/EFM32G/Include/system_efm32g.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_aes.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_dma_ch.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_msc.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_emu.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_rmu.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_timer_cc.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_timer.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_usart.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_leuart.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_rtc.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_letimer.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_pcnt.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_acmp.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_prs_ch.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_dac.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_gpio_p.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_gpio.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_vcmp.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_adc.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_i2c.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_wdog.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_dma_descriptor.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_devinfo.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_romtable.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_calibrate.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_dmareq.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_dmactrl.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_af_ports.h \
+ ../Device/EnergyMicro/EFM32G/Include/efm32g_af_pins.h \
+ ../emlib/inc/em_bitband.h ../emlib/inc/em_assert.h
+
+../emlib/inc/em_gpio.h:
+
+../Device/EnergyMicro/EFM32G/Include/em_device.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g230f128.h:
+
+../CMSIS/Include/core_cm3.h:
+
+../CMSIS/Include/core_cmInstr.h:
+
+../CMSIS/Include/core_cmFunc.h:
+
+../Device/EnergyMicro/EFM32G/Include/system_efm32g.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_aes.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_dma_ch.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_msc.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_emu.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_rmu.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_timer_cc.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_timer.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_usart.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_leuart.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_rtc.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_letimer.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_pcnt.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_acmp.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_prs_ch.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_dac.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_gpio_p.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_gpio.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_vcmp.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_adc.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_i2c.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_wdog.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_dma_descriptor.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_devinfo.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_romtable.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_calibrate.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_dmareq.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_dmactrl.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_af_ports.h:
+
+../Device/EnergyMicro/EFM32G/Include/efm32g_af_pins.h:
+
+../emlib/inc/em_bitband.h:
+
+../emlib/inc/em_assert.h:
